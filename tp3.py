5bdd71f3cb4fadbfdcbd1fc7848f32b36c1b2544
Analia92-franco <analia.franco@tupad.utn.edu.ar>
2025-08-23 18:33:31 -0300

    Trabajo Práctico, unidad 3 - Estructuras Condicionales

---
+++ tp3.py
@@ -0,0 +1,41 @@
+# #Ejercicio1
+# edad = int(input("Ingrese su edad: "))
+
+# if edad >= 18:
+#     print("Es mayor de edad")
+# elif edad < 18:
+#     print("Es menor de edad")
+
+#Ejercicio2
+# nota = float(input("Ingrese la nota: "))
+
+# if nota >= 6:
+#     print("APROBADO")
+# elif nota < 6:
+#     print("DESAPROBADO")
+
+#Ejercicio3
+# num = int(input("Por favor, ingrese un número par: "))
+
+# if num % 2 == 0:
+#     print(f"El número {num} es par")
+# elif num != 0:
+#     print(f"El número {num} es impar")
+
+#Ejercicio4
+# edad = int(input("Ingrese su edad: "))
+
+# if edad < 12:
+#     print("La categoría es Niño/a")
+# elif edad >= 12 and edad < 18:
+#     print("La categoría es Adolescente")
+# elif edad >= 18 and edad < 30:
+#     print("La categoría es Adulto/a joven")
+# elif edad >= 30:
+#     print("La categoría es Adulto/a")
+
+#Ejercicio5
+password = int(input("Por favor, ingrese una contraseña entre 8 y 14 caracteres: "))
+#Como ingresar para letras y números?
+if (password >= 6 and <= 16):
+    print("Ha ingresado una contraseña correcta")
